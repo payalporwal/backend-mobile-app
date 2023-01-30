468a635733a6fa8d284ca87bdd7f15f3f8f6b55d
payal <payalporwal06@gmail.com>
2023-01-30 13:27:46 +0530

    header

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
+    res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     next();
   });
