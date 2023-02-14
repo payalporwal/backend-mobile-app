b4e028d94444930b4383b96484cb4f258b3592fd
payal <payalporwal06@gmail.com>
2023-02-14 16:47:42 +0530

    env changes

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 let environments = {};
 
-environments.test = {
+environments.development = {
     HOST : '0.0.0.0',
     PORT : 3000,
     DB : process.env.DB_URL_DEV 
 }
-environments.development = {
-    HOST : process.env.HOST,
+environments.test = {
+    HOST : process.env.HOST_DEV,
     PORT : process.env.PORT_DEV,
     DB : process.env.DB_URL_DEV 
 }
 environments.production = {
-    HOST : process.env.HOST,
+    HOST : process.env.HOST_PROD,
     PORT : process.env.PORT_PROD,
     DB : process.env.DB_URL_PROD, 
 }
