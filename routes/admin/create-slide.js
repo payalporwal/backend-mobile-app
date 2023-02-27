2a00cea5e827776966f6646c8242b2e453c9f8d3
payal <payalporwal06@gmail.com>
2023-02-28 01:10:51 +0530

    check again

--- routes/admin/create-slide.js
+++ routes/admin/create-slide.js
@@ -28,7 +28,7 @@ router.post('/create-slide', async (req, res, next) => {
 // get slide data
 router.get('/get-slide', async (req, res, next) => {
     try{
-        const slide = await Slide.find().select('slidedata');
+        const slide = await Slide.find();
         res.status(200).json({slide: slide});
     } catch(err){
         console.log(err);
