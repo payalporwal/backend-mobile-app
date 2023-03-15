b18c1aca1f0c2110dc809dc8767fba2a453a2ba5
payal <payalporwal06@gmail.com>
2023-03-16 00:11:25 +0530

    commit

--- middleware/multer.js
+++ middleware/multer.js
@@ -14,15 +14,7 @@ const fileUpload = multer({
   limits: {
     fileSize: 2*1024*1024,
   },
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, `uploads/images`);
-    },
-    filename: (req, file, cb) => {
-      const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, `${timeStamp.toISOString()}-${file.fieldname}-pace.${ext}`);
-    }
-  }),
+  storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
     let error = isValid ? null : new HttpError('Invalid image type!,Only jpg, jpeg, png, gif files are allowed', false , 400);
