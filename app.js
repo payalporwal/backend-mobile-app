e2aa59dee8a8f04910b3e0af0602c19a62491c78
payal <payalporwal06@gmail.com>
2023-03-17 16:50:04 +0530

    cors

--- app.js
+++ app.js
@@ -37,7 +37,11 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 // Allow Cross-Origin requests
-app.use(cors());
+const corsOptions ={
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // Set security HTTP headers
 app.use(helmet());
