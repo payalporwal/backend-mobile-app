58450147bad1e8b91a5ce5fda5b8e7bd3185b32d
payal <payalporwal06@gmail.com>
2023-03-17 16:54:28 +0530

    cors

--- app.js
+++ app.js
@@ -55,7 +55,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "500");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
   res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
   next();
 });
