e3749a13bda74b919ee5b51f4a3679ed8453fcc1
payal <payalporwal06@gmail.com>
2023-03-19 19:54:48 +0530

    changes

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res, next) => {
-  res.send('Welcome to Pace!!!')
+  res.send('Welcome to Pace!! Testing')
 });
 
 app.use('/uploads', express.static('uploads'));
